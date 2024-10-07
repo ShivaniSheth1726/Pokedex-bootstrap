bdfa2f63cdee420b9c3958dff3acc50beb661f58
Shivani Sheth Mehta <shethshivani1726@gmail.com>
2024-10-07 07:34:04 +0000

    style added

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,30 @@ import data from "./data";
 import shuffle from "array-shuffle";
 
 // Component
-import PokemonCard from "./components/pokemonCard";
+import PokemonCard from "./components/PokemonCard";
 
 // === DOM Targeting ===
 const inputEl = document.querySelector('input[type="text"]');
 const dataRow = document.querySelector("[data-row]");
 
-renderPokemon(shuffle(data));
+renderPokemon(shuffle(data)); //shuffle data on every refresh
 
 // Give list, it will render them
 function renderPokemon(list) {
   dataRow.textContent = "";
-
+// to show no result found when result is 0
   if (list.length === 0) {
     const noResultsMessage = document.createElement("div");
     noResultsMessage.textContent = "No results found.";
     dataRow.appendChild(noResultsMessage);
     return;
   }
-
+// to iterate and show each element
   list.forEach((pokemonObj) => {
     PokemonCard(pokemonObj);
   });
 }
-
+// Apply search functionality in bothcase
 function handleSearch(input) {
   const filteredPokemon = data.filter((pokemonObj) =>
     pokemonObj.name.toLowerCase().includes(input)
@@ -43,7 +43,7 @@ inputEl.addEventListener("input", (e) => {
 document.addEventListener("keydown", (e) => {
   if (e.key === "/") {
     // Don't type
-    e.preventDefault();
+    e.preventDefault(); //after pressing / dont apper on the search bar
     inputEl.focus();
   }
 });

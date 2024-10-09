f78089862a41c167d799b46e60087f91fad14625
Shivani Sheth Mehta <shethshivani1726@gmail.com>
2024-10-09 15:56:17 +0000

    transpiling

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,54 +1,89 @@
-// node imports
+// Packages
 import shuffle from "array-shuffle";
-// data imports 
-import data from "./data.json";
-// Component imports
-import PokemonCard from "./components/PokemonCard";
+import Fuse from "fuse.js";
+
+// Imports
+import "../scss/main.scss"; // Styling
+import data from "./data.json"; // Data
+import PokemonCard from "./components/PokemonCard"; // Component
+
+// Custom type representing a pokemon object
+interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  link: string;
+  abilities: string[];
+}
 
 // === DOM Targeting ===
-const inputEl = document.querySelector('input[type="text"]');
-const dataRow = document.querySelector("[data-row]");
+const inputEl = document.querySelector(
+  'input[type="text"]'
+) as HTMLInputElement;
+const dataRow = document.querySelector("[data-row]") as HTMLDivElement;
 
-renderPokemon(shuffle(data)); //shuffle data on every refresh
+renderPokemon(shuffle(data));
 
 // Give list, it will render them
-function renderPokemon(list) {
+function renderPokemon(list: Pokemon[]): void {
   dataRow.textContent = "";
-// to show no result found when result is 0
-  if (list.length === 0) {
-    const noResultsMessage = document.createElement("div");
-    noResultsMessage.textContent = "No results found.";
-    dataRow.appendChild(noResultsMessage);
-    return;
+
+  if (!list.length) {
+    const pokemon = PokemonCard({
+      name: "Not found",
+      description: "Try another search",
+      image:
+        "https://i.pinimg.com/originals/f8/29/be/f829bed61f75627eea111dfde089fe2c.png",
+    });
+    dataRow.appendChild(pokemon);
   }
-// to iterate and show each element
-  list.forEach((pokemonObj) => {
-    const pokemon=PokemonCard(pokemonObj);
+
+  list.forEach((pokemonObj: Pokemon) => {
+    const pokemon = PokemonCard(pokemonObj);
     dataRow.appendChild(pokemon);
   });
 }
-// Apply search functionality in bothcase
-function handleSearch(input) {
-  const filteredPokemon = data.filter((pokemonObj) =>{
-   return pokemonObj.name.toLowerCase().includes(input)
-});
 
-  renderPokemon(filteredPokemon);
+// Will be invoked on search
+function handleSearch(input: string): void {
+  // Create fuse object
+  const options = {
+    keys: ["name", "abilities"],
+    threshold: 0.5,
+  };
+  const fuse = new Fuse(data, options);
+
+  // Perform search
+  function performSearch(): Pokemon[] {
+    if (!input) return data;
+
+    const searched = fuse.search(input);
+    return searched.map((obj) => obj.item);
+  }
+
+  // Create without the 'item' key from fuse search
+  const filterdPokemon = performSearch();
+  renderPokemon(filterdPokemon);
 }
 
-inputEl.addEventListener("input", (e) => {
-  // console.log(e); //log the key pressed 
-  // console.log('hey'); //log the word when key pressed
-  
-  const currentInput = e.target.value.trim().toLowerCase();
-  handleSearch(currentInput);
+let debounceTimer: ReturnType<typeof setTimeout>;
+inputEl.addEventListener("input", (e: Event) => {
+  clearTimeout(debounceTimer);
+
+  const target = e.target as HTMLInputElement;
+
+  debounceTimer = setTimeout(() => {
+    const currentInput = target.value.trim().toLowerCase();
+    handleSearch(currentInput);
+  }, 400);
 });
 
 // Add / to active search
-document.addEventListener("keydown", (e) => {
+document.addEventListener("keydown", (e: KeyboardEvent) => {
   if (e.key === "/") {
     // Don't type
-    e.preventDefault(); //after pressing / dont apper on the search bar
+    e.preventDefault();
     inputEl.focus();
   }
 });
